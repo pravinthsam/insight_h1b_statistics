18f01cf65dc9deacff163daa5898fc3dad97ed36
Pravinth Samuel Vethanayagam <pravinth.samuel@gmail.com>
2018-10-30 11:29:58 -0400

    Added code to count the SOC_CODE and then map it to SOC_NAME

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -1,68 +1,79 @@
 import sys
 import re
 
-# The python file requires 3 arguments
-if len(sys.argv) != 4:
-    raise Exception('The python file requires exactly 3 arguments')
-    
 in_file = sys.argv[1]
 occupation_out_file = sys.argv[2]
 state_out_file = sys.argv[3]
+onet_file = sys.argv[4]
+soc_file = sys.argv[5]
 
-# We keep track of occupation name counts and state counts using dicts
-occupation_counter = {}
+occupation_name_counter = {}
 state_counter = {}
 total_certified = 0.0
 
-with open(in_file) as f:
-    for i, line in enumerate(f):
+# Create a dict of onet_code to soc_name
+onet_to_name = {}
+with open(onet_file, encoding='utf8') as f:
+    for line in f:
+        onet_code, name = line.strip().split(';')
+        onet_to_name[onet_code] = name.upper()
+
+# Create a dict of soc_code to soc_name
+soc_to_name = {}
+with open(soc_file, encoding='utf8') as f:
+    for line in f:
+        soc_code, name = line.strip().split(';')
+        soc_to_name[soc_code] = name.upper()
         
-        # Split with semi-colon as the delimiter
+
+with open(in_file, encoding='utf8') as f:
+    for i, line in enumerate(f):
         fields = line.split(';')
         
-        # Header row
         if i==0:
-            # The field names can change based on year. So we check for all the know aliases as well.
-            occupation_field_index = [fields.index(field) for field in fields if field in {'SOC_NAME', 'LCA_CASE_SOC_NAME'}][0]
+            occupation_code_field_index = [fields.index(field) for field in fields if field in {'SOC_CODE', 'LCA_CASE_SOC_CODE'}][0]
+            occupation_name_field_index = [fields.index(field) for field in fields if field in {'SOC_NAME', 'LCA_CASE_SOC_NAME'}][0]
             certified_field_index = [fields.index(field) for field in fields if field in {'CASE_STATUS', 'STATUS'}][0]
             state_field_index = [fields.index(field) for field in fields if field in {'WORKSITE_STATE', 'LCA_CASE_WORKLOC1_STATE'}][0]
-        # If not header
+            relevant_field_indices = [
+                                        occupation_code_field_index,
+                                        state_field_index
+                                     ]
         else:
-            # we should process only certified rows
             if fields[certified_field_index] == 'CERTIFIED':
+                occupation_code = re.sub(r'\"', '', fields[occupation_code_field_index])
                 
-                # Retrieve occupation and state and clean it up
-                occupation = re.sub(r'\"', '', fields[occupation_field_index])
+                occupation_name = re.sub(r'\"', '', fields[occupation_name_field_index]).upper()
+                if occupation_code in soc_to_name.keys():
+                    occupation_name = soc_to_name[occupation_code]
+                elif occupation_code in onet_to_name.keys():
+                    occupation_name = onet_to_name[occupation_code]
+                
                 state = re.sub(r'\n', '', fields[state_field_index])
                 
                 total_certified += 1.0
                 
-                # Add to occupation counter
-                if occupation in occupation_counter:
-                    occupation_counter[occupation] += 1
+                if occupation_name in occupation_name_counter:
+                    occupation_name_counter[occupation_name] += 1
                 else:
-                    occupation_counter[occupation] = 1
+                    occupation_name_counter[occupation_name] = 1
                     
-                # Add to state counter
                 if state in state_counter:
                     state_counter[state] += 1
                 else:
                     state_counter[state] = 1
 
-# Now we need to sort the counts based on the count(descending) and the occupation name (ascending)                
-occupation_tuples = sorted(occupation_counter.items(), key = lambda x: (-x[1], x[0]))
+                   
+occupation_tuples = sorted(occupation_name_counter.items(), key = lambda x: (-x[1], x[0]))
 
-# We will now write only the top 10 records after sorting
 with open(occupation_out_file, 'w') as f:  
     f.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
-    for occupation, num in occupation_tuples[:10]:
-        f.write( '{};{};{:.1%}\n'.format(occupation, num, num/total_certified) )
+    for occupation_name, num in occupation_tuples[:10]:
+        f.write( '{};{};{:.1%}\n'.format(occupation_name, num, num/total_certified) )
 
-# Similarly for states
 state_tuples = sorted(state_counter.items(), key = lambda x: (-x[1], x[0]))
 
 with open(state_out_file, 'w') as f:  
     f.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
     for state, num in state_tuples[:10]:
         f.write( '{};{};{:.1%}\n'.format(state, num, num/total_certified) )
-        
